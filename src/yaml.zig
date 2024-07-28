59ca9ab3b615373f35f71639ddec0a17c8763197
Charlie Schoettle <cschoettle981@gmail.com>
2024-07-28 14:13:28 -0400

    added yaml definitions

---
+++ src/yaml.zig
@@ -0,0 +1,30 @@
+const std = @import("std");
+
+pub const YAMLError = error{
+    UnsupportedSyntax,
+};
+
+pub const YAMLScalarTag = enum {
+    integer,
+    float,
+    string,
+    boolean,
+    null,
+};
+
+const YAMLDataTag = enum {
+    scalar,
+    sequence,
+    mapping,
+};
+
+pub const YAMLData = union(YAMLDataTag) {
+    scalar: union(YAMLScalarTag) {
+        integer: i64,
+        float: f64,
+        string: []const u8,
+        boolean: bool,
+    },
+    sequence: std.ArrayList(YAMLData),
+    mapping: std.StringHashMap(YAMLData),
+};
