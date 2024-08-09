ad347ecebc5227db52db27b1914813b14588c675
Charlie Schoettle <cschoettle981@gmail.com>
2024-08-09 17:55:46 -0400

    added installation of test exe

--- build.zig
+++ build.zig
@@ -24,10 +24,13 @@ pub fn build(b: *std.Build) void {
     check.dependOn(&exe_check.step);
 
     const exe_unit_tests = b.addTest(.{
+        .name = "tests",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
+
+    b.installArtifact(exe_unit_tests);
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
